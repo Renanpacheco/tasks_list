4600194d910a6acf9bfbe05cc7d9175d4bccadae
Renan Bitencourt Pacheco <prenan86@gmail.com>
2024-12-03 09:23:24 -0300

    Update taskRoutes.js

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const TaskController = require('../controllers/taskController')
 
 router.post('/create', TaskController.createTask)
-router.get('/', TaskController.getTasks)
+router.get('/api', TaskController.getTasks)
 router.delete('/remove/:id', TaskController.deleteTask)
 router.put('/update/:id', TaskController.updateTask)
 router.put("/change", TaskController.updateOrder);
